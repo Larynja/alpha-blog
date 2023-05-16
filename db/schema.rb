720d3e6b09481c75e9f6d398ec3e3f9ad48ca4ce
Larynja <ddombrowski.py@gmail.com>
2023-05-16 16:12:37 +0200

    added show articles

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_15_153523) do
   create_table "articles", force: :cascade do |t|
-    t.string "titile"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
